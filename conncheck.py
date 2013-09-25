1cc8e07f5ebc89c77d9fb15750fd5ad12689afb7
Moritz Schlarb <mail@moritz-schlarb.de>
2013-09-25 20:18:13 +0200

    conncheck

--- conncheck.py
+++ conncheck.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import sys
+
+from connman import isLoggedIn
+
 if __name__ == '__main__':
-	pass
+	status = isLoggedIn()
+	print status
+	sys.exit(0 if status else 1)
